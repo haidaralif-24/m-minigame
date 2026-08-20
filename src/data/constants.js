5d97744c54481f53ed48d56ef323fdfa22659a6e
Haidar Ali Fawwaz Nasirodin <haidar.nasirodin@gdajogja.sch.id>
2026-08-20 09:52:55 +0700

    Switch game configuration from six teams to seven players

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,34 +2,11 @@ import meta from '../content/maulid-nabi/meta.json';
 
 export const ACTIVE_EVENT = meta.id;
 export const ACTIVE_META = meta;
-
-export const NUM_TEAMS = 6;
-export const TEAM_NAMES = ['Team 1', 'Team 2', 'Team 3', 'Team 4', 'Team 5', 'Team 6'];
-export const TEAM_IDS = ['team1', 'team2', 'team3', 'team4', 'team5', 'team6'];
-export const HOST_PIN = 'dadarzz';
-export const TEAM_KEYS = ['one', 'two', 'three', 'four', 'five', 'six'];
+export const NUM_PLAYERS = 7;
+export const MAX_PLAYERS = 7;
 export const BOARD_LENGTH = 31;
 export const DICE_SIZE = 6;
-
-export const TOKEN_COLORS = [
-  '#ef4444', // red
-  '#22c55e', // green
-  '#3b82f6', // blue
-  '#f59e0b', // amber
-  '#a855f7', // purple
-  '#ec4899', // pink
-];
-
-export const PHASES = {
-  LOBBY: 'lobby',
-  BOARD: 'board',
-  FINISHED: 'finished',
-};
-
-export const TILE_TYPES = {
-  START: 'start',
-  NORMAL: 'normal',
-  BONUS: 'bonus',
-  PENALTY: 'penalty',
-  FINISH: 'finish',
-};
+export const HOST_PIN = 'dadarzz';
+export const TOKEN_COLORS = ['#ef4444', '#22c55e', '#3b82f6', '#f59e0b', '#a855f7', '#ec4899', '#14b8a6'];
+export const PHASES = { LOBBY: 'lobby', RAPID_SHOT: 'rapid-shot', ORDER_REVEAL: 'order-reveal', BOARD: 'board', FINISHED: 'finished' };
+export const TILE_TYPES = { START: 'start', NORMAL: 'normal', BONUS: 'bonus', PENALTY: 'penalty', FINISH: 'finish' };
