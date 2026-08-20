6fcb1ac6dc502461eb88cdf962ab825aa9e148bd
Haidar Ali Fawwaz Nasirodin <haidar.nasirodin@gdajogja.sch.id>
2026-08-20 09:55:00 +0700

    Route the spectator board through the new multiplayer room host view

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LobbyPage from './pages/Lobby.jsx';
 import HostPage from './pages/Host.jsx';
-import BoardPage from './pages/Board.jsx';
 import PlayPage from './pages/Play.jsx';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LobbyPage />} />
-        <Route path="/lobby" element={<LobbyPage />} />
-        <Route path="/host" element={<HostPage />} />
-        <Route path="/board" element={<BoardPage />} />
-        <Route path="/play" element={<PlayPage />} />
-        <Route path="/*" element={<LobbyPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <BrowserRouter><Routes>
+    <Route path="/" element={<LobbyPage />} />
+    <Route path="/lobby" element={<LobbyPage />} />
+    <Route path="/host" element={<HostPage />} />
+    <Route path="/board" element={<HostPage />} />
+    <Route path="/play" element={<PlayPage />} />
+    <Route path="*" element={<Navigate to="/" replace />} />
+  </Routes></BrowserRouter>;
 }
